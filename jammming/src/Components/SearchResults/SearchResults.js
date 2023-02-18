f4b862fbbeb3d0c08760ee957ad72feea1292580
lordTocho <brian.highnam@accenturefederaldefense.com>
2023-02-18 11:40:12 -0500

    added in SearchBar and SearchResult components

---
+++ jammming/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SearchResults.css';
+
+export class SearchResults extends React.Component {
+    render() {
+        return <div className="SearchResults">
+            <h2>Results</h2>
+            Add a TrackList component
+        </div>
+    }
+}
