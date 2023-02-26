15ea93af5939113b3a26c71263755eabf1fe1945
lordTocho <brian.highnam@accenturefederaldefense.com>
2023-02-26 12:06:05 -0500

    passed 35

--- jammming/src/Components/TrackList/TrackList.js
+++ jammming/src/Components/TrackList/TrackList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./TrackList.css";
-
+import { Track } from "../Track/Track";
 export class TrackList extends React.Component {
     
   render() {
    console.log( this.props.tracks )
-    return <div className="TrackList">
+   
+    return ( <div className="TrackList">
+            {
+              this.props.tracks.map( track => {
+                return <Track track={track} key={track.id}/>
+              } )
+            }
           </div>
+    )
   }
 }
