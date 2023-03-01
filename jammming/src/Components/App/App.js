b713488bb4ea68f9143bd31d2375b3bd3e7844f6
lordTocho <brian.highnam@accenturefederaldefense.com>
2023-02-28 20:20:45 -0500

    Finally passed step 40!

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -15,6 +15,27 @@ class App extends React.Component {
           artist: "Artist",
           album: "Album 1",
         }
+      ],
+      playlistName: "Metal",
+      playlistTracks: [
+        {
+          id: 1,
+          name: "Metal Doe",
+          artist: "Metal Smith",
+          album: "Metal Album"
+        },
+        {
+          id: 2,
+          name: "Metal Doe2",
+          artist: "Metal Smith2",
+          album: "Metal Album2"
+        },
+        {
+          id: 3,
+          name: "Metal Doe3",
+          artist: "Metal Smith3",
+          album: "Metal Album3"
+        }
       ]
     };
   
@@ -26,7 +47,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <PlayList searchResults={this.state.searchResults} />
+            <PlayList searchResults={this.state.searchResults} playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
     </div>
--- jammming/src/Components/Playlist/Playlist.js
+++ jammming/src/Components/Playlist/Playlist.js
@@ -5,7 +5,7 @@ export class PlayList extends React.Component {
     render() {
         return <div className="Playlist">
                     <input defaultValue={'New Playlist'}/>
-                        <TrackList />
+                        <TrackList tracks={this.props.playlistTracks}/>
                     <button className="Playlist-save">SAVE TO SPOTIFY</button>
                 </div>
     }
